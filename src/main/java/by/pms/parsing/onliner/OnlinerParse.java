259dce44c162b917c4edd72937d4824673a07fde
Andrey <55462720+SweatyJoe@users.noreply.github.com>
2023-04-20 21:05:17 +0300

    start using selenium

--- src/main/java/by/pms/parsing/onliner/OnlinerParse.java
+++ src/main/java/by/pms/parsing/onliner/OnlinerParse.java
@@ -1,9 +1,12 @@
 package by.pms.parsing.onliner;
 
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Request;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,14 +31,28 @@ public class OnlinerParse {
             int pageIterator = 0;
             int lastPage = 1;
             for (; pageIterator < lastPage; pageIterator++) {
-                Content content = Request.Get("https://app.scrapingbee.com/api/v1/?api_key=NXJ64ADCJ2ZMX77CATOTMGY2INAHJZEBQUDWPDZFDRRRNWFLRYPPF4YQFAL80RC1739BC9KHCNVVUDDV&url=" +
+                System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
+
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+
+                WebDriver wDriver = new ChromeDriver(options);
+                wDriver.get(urlGen(pageIterator, "cpu"));
+
+                Document doc = Jsoup.parse(wDriver.getPageSource());
+                Elements elements = doc.select("schema-pagination__pages-link");
+                for(Element s : elements){
+                    System.out.println(s.text());
+                }
+                /*Content content = Request.Get("https://app.scrapingbee.com/api/v1/?api_key=NXJ64ADCJ2ZMX77CATOTMGY2INAHJZEBQUDWPDZFDRRRNWFLRYPPF4YQFAL80RC1739BC9KHCNVVUDDV&url=" +
                                 urlGen(pageIterator, "cpu"))
                         .execute().returnContent();
-                lastPage = content.toString().lastIndexOf("schema-pagination__pages-link"); //find last page
+                */
+                //lastPage = content.toString().lastIndexOf("schema-pagination__pages-link"); //find last page
 
                 //save page as html-file
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
