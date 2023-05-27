01b93139a61cb870616f7456d1a737e966c04b36
Andrey <55462720+SweatyJoe@users.noreply.github.com>
2023-05-27 22:48:43 +0300

    html reworks

---
+++ src/main/java/by/pms/parsing/prooffice/ProofficeParseThread.java
@@ -0,0 +1,14 @@
+package by.pms.parsing.prooffice;
+
+public class ProofficeParseThread {
+    private final String[] COMPONENTS = {
+            "protsessory",
+            "bloki-pitaniya",
+            "videokarty",
+            "zhestkie-diski",
+            "materinskie-platy",
+            "moduli-pamyati",
+            "ssd",
+
+    };
+}
